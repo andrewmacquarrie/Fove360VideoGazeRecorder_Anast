c5176c27593f52a12cc9283cde68daa3e7802531
Drew <andrew.macquarrie@gmail.com>
2019-05-28 12:37:04 +0100

    deactivating cues when the eye location is withing a certain angle of the target

--- Assets/DeactivateWithinAngleToTarget.cs
+++ Assets/DeactivateWithinAngleToTarget.cs
@@ -7,6 +7,7 @@ public class DeactivateWithinAngleToTarget : MonoBehaviour {
 	public float deactivationAngle;
 	public GameObject objectToDeactivate;
 	public GameObject eyeLocation;
+	public GameObject target;
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +17,12 @@ public class DeactivateWithinAngleToTarget : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var vectorToEyePosition = eyeLocation.transform.position;
+		var vectorToTarget = target.transform.position;
+		if(Vector3.Angle(vectorToEyePosition, vectorToTarget) < deactivationAngle){
+			objectToDeactivate.SetActive(false);
+		} else {
+			objectToDeactivate.SetActive(true);
+		}
+		
 	}
 }
