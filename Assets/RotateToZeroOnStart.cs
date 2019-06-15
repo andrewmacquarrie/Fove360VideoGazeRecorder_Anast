4cd9909fd3bf8279b606981c3f8b20c766e9e57b
localadmin@euro <localadmin@euro>
2019-06-15 15:36:09 +0100

    attempting to make FOVE point forward in video on load, but I dont think it works

---
+++ Assets/RotateToZeroOnStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToZeroOnStart : MonoBehaviour {
+
+
+    private GameObject mainCamera;
+    
+	// Use this for initialization
+	void Start () {
+
+        //mainCamera = GameObject.Find("FOVE Eye (Left)");
+
+        if (mainCamera == null)
+        { // the FOVE isn't connected - just use the regular camera
+            mainCamera = GameObject.Find("Fove Interface");
+        }
+        
+//        Debug.LogError(mainCamera.transform.rotation.eulerAngles.y);
+        transform.rotation = Quaternion.Euler(0f, 200f - mainCamera.transform.localRotation.eulerAngles.y, 0f);
+ //       Debug.LogError(mainCamera.transform.rotation.eulerAngles.y);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+}
